1f375028fddf807190db401e36f0a1247ea845c7
Strelko11 <adam.strelec.11@gmail.com>
2025-02-26 15:46:04 +0100

    Skip commit

--- WTECH/resources/views/welcome.blade.php
+++ WTECH/resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HoleCicky.sk</title>
+    <title>Welcome</title>
 </head>
 <body>
     <header>
-        <h1>HolePipiky</h1>
+        <h1>Welcome hihi</h1>
         <nav>
             <ul>
                 <li><a href="#">Home</a></li>
